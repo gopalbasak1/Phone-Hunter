84e77eacd5afb783198421a5b49c2878873990cc
gopalbasak1 <gopalbasak1223@gmail.com>
2024-03-06 09:32:18 +0600

    fetch phone api and show card

---
+++ js/phone.js
@@ -0,0 +1,42 @@
+const loadPhone = async() =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phones = data.data;
+
+    //console.log(phones);
+    displayPhones(phones)
+}
+
+const displayPhones = phones =>{
+    //console.log(phones);
+    const phoneContainer = document.getElementById('phone-container');
+
+    phones.forEach(phone =>{
+        console.log(phone);
+
+        const phoneCard = document.createElement('div');
+        phoneCard.classList= `card  bg-base-100 shadow-xl p-4`;
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.phone_name
+          }</h2>
+          <p>${phone.brand}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+    })
+}
+
+//handle search button
+const handleSearch = ()=>{
+  const searchField = document.getElementById('input-field').value;
+  console.log(searchField);
+}
+
+
+
+loadPhone();
